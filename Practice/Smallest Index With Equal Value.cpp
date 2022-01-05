e11718a15923921d0f944b654b9bbec3e9c28fd6
aforarpitkumar <80710050+aforarpitkumar@users.noreply.github.com>
2022-01-05 12:04:59 +0530

    Update Smallest Index With Equal Value.cpp

--- Practice/Smallest Index With Equal Value.cpp
+++ Practice/Smallest Index With Equal Value.cpp
@@ -4,21 +4,24 @@ class Solution {
 public:
     int smallestEqual(vector<int>& nums) {
         
-        vector<int> ans ;
-        int temp =0;
-        int ans1 =0;
+        int ans =0;
         
         for (int i=0; i<nums.size(); i++){
-         
-            if (nums[i]%10==nums[i]){
-                ans.push_back(i);
+            
+            if (nums[i]==i%10){
+                
+                ans = i;
+                
+                break;
+            }
+            
+            else{
+                ans = -1;
             }
             
         }
         
-        //sort(ans.begin(),ans.end());
-        
-        return ans[0];
+        return ans;
         
     }
 };
